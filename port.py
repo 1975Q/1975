dc1b011cc7927e1920d37f872b695b79d7868e27
Al Bri6any 1975 <143816351+TurkyMc@users.noreply.github.com>
2024-08-14 20:47:11 +0300

    Add files via upload

---
+++ port.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+  
+import socket 
+
+sock = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+
+ip = "31.13.69.35"
+
+port = 443
+
+if sock.connect_ex((ip , port))== 0:
+  print ("port  is open")
+else:
+  print ("port 443 is closed")
+
